23bca0dd47abad942cc079441b6359208e58fa81
CasaNovack <92743291+CasaNovack@users.noreply.github.com>
2021-11-19 17:10:17 +0100

    Add files via upload

---
+++ zad7.cpp
@@ -0,0 +1,30 @@
+﻿// zad7.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int suma=0;
+	int tab[4][4] = {1,6,2,8,3,4,9,7,5,6,2,9,8,4,3,6 };
+
+
+	for (int i = 0; i < 4; i++)
+	{
+			suma = suma + tab[i][i];
+	}
+	cout << suma;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
